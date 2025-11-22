680c9ac3a86125fc136791dbc7677ec36ef38781
Your Name <thanhtoana7hha@gmail.com>
2025-11-22 07:34:47 +0700

    them category

--- js/script.js
+++ js/script.js
@@ -15,3 +15,34 @@ itemSliderBar.forEach(function (menu, index) {
     menu.classList.toggle("block");
   });
 });
+//------------------------filter----------------
+const btnFilter = document.querySelector(".btn-filter");
+const filterSlider = document.querySelector(".catergory-filter-content");
+const chooseSize = document.querySelectorAll(
+  ".catergory-filter-content-size-choose"
+);
+const chooseColor = document.querySelectorAll(
+  ".catergory-filter-content-color-choose"
+);
+const btnRemove = document.querySelector(
+  ".catergory-filter-content-bottom-remove"
+);
+
+btnFilter.addEventListener("click", function () {
+  filterSlider.classList.toggle("block");
+});
+
+chooseSize.forEach(function (choose, index) {
+  choose.addEventListener("click", function () {
+    choose.classList.toggle("choose");
+  });
+});
+chooseColor.forEach(function (choose, index) {
+  choose.addEventListener("click", function () {
+    choose.classList.toggle("choose");
+  });
+});
+btnRemove.addEventListener("click", function () {
+  chooseSize.forEach((item) => item.classList.remove("choose"));
+  chooseColor.forEach((item) => item.classList.remove("choose"));
+});
