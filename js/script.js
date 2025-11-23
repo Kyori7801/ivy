7b46a4054a35891bf3ae4c86ca485086df2e88cf
Your Name <thanhtoana7hha@gmail.com>
2025-11-24 02:06:15 +0700

    them product va cart

--- js/script.js
+++ js/script.js
@@ -28,9 +28,11 @@ const btnRemove = document.querySelector(
   ".catergory-filter-content-bottom-remove"
 );
 
-btnFilter.addEventListener("click", function () {
+if(btnFilter){
+  btnFilter.addEventListener("click", function () {
   filterSlider.classList.toggle("block");
 });
+}
 
 chooseSize.forEach(function (choose, index) {
   choose.addEventListener("click", function () {
@@ -42,7 +44,53 @@ chooseColor.forEach(function (choose, index) {
     choose.classList.toggle("choose");
   });
 });
-btnRemove.addEventListener("click", function () {
+if(btnRemove){
+  btnRemove.addEventListener("click", function () {
   chooseSize.forEach((item) => item.classList.remove("choose"));
   chooseColor.forEach((item) => item.classList.remove("choose"));
 });
+}
+
+//--------------------- product----------------
+ const bigImg= document.querySelector(".product-content-left-big-img img")
+ const smallImg = document.querySelectorAll(".product-content-left-small-img img")
+
+ const baoQuan= document.querySelector(".product-content-right-bottom-content-tille-item.baoquan")
+ const chiTiet =document.querySelector(".product-content-right-bottom-content-tille-item.gioithieu")
+ const btnDetals = document.querySelector(".product-content-right-bottom-top") 
+
+
+  smallImg.forEach(function(imgItem,X){
+    imgItem.addEventListener("click",function(){
+      bigImg.src= imgItem.src
+    })
+  })
+
+
+ if(baoQuan || chiTiet || btnDetals){
+ 
+
+
+
+
+  document.querySelector(".product-content-right-bottom-content-details-baoquan").style.display="none";
+  baoQuan.addEventListener("click",function(){
+    document.querySelector(".gioithieu").classList.remove("active")
+    document.querySelector(".baoquan").classList.add("active")
+    document.querySelector(".product-content-right-bottom-content-details-gioithieu").style.display="none";
+    document.querySelector(".product-content-right-bottom-content-details-baoquan").style.display="block";
+  })
+
+  chiTiet.addEventListener("click",function(){
+     document.querySelector(".baoquan").classList.remove("active")
+    document.querySelector(".gioithieu").classList.add("active")
+    document.querySelector(".product-content-right-bottom-content-details-baoquan").style.display="none";
+    document.querySelector(".product-content-right-bottom-content-details-gioithieu").style.display="block";
+  })
+
+  btnDetals.addEventListener("click",function(){
+    document.querySelector(".product-content-right-bottom-content").classList.toggle("deactive-details")
+  })
+ }
+
+
